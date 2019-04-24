14565b0759ef7a73d6ddec30aac6e86659f3be27
Aaron Pahwa <a.pahwa.m@gmail.com>
2019-04-24 17:13:25 -0400

    Simple front end

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ track the B11 at the 306582 stop at
 """
 
 """
-Future Patches:
+Future Versions:
 --> Next Bus
 --> Specify what line and stop you want
     --> Use maps api and location data to find closest stop to your line
 """
 
-# [START python_app]
-
 import requests
 import json
-from flask import Flask, make_response, request, jsonify
+import datetime
+import time
+import iso8601
+import math
+from flask import Flask, make_response, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -27,22 +29,35 @@ def get_request1():
     action = req.get('queryResult').get('action')
 
     if action == "get_distance":
+        #API GET:
+        
         payload = {'key':'3a03f90d-7ca3-4b74-b22f-8b6535e6d788','OperatorRef':'MTA','MonitoringRef':'306582','Li':'NYCT_B11','StopMonitoringDetailLevel':'basic','MaximumStopVisits':'1','MinimumStopVisitsPerLine':'1'}
         obj = requests.get('http://bustime.mta.info/api/siri/stop-monitoring.json', params=payload)
         res = obj.json()
+
+        
         distance = res['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
-        payloadStops = {'key':'3a03f90d-7ca3-4b74-b22f-8b6535e6d788','OperatorRef':'MTA','MonitoringRef':'306582','Li':'NYCT_B11','StopMonitoringDetailLevel':'basic','MaximumStopVisits':'1','MinimumStopVisitsPerLine':'1'}
-        objStops = requests.get('http://bustime.mta.info/api/siri/stop-monitoring.json', params=payload)
-        resStops = obj.json()
         stops = res['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['StopsFromCall']
+        busTime = res['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']["ExpectedArrivalTime"]
+
+        #Time parsing:
+
+        arrival_time = time.mktime(iso8601.parse_date(busTime).timetuple())
+        current_time = time.mktime(datetime.datetime.now().timetuple())
+        eta = math.floor(math.floor((arrival_time - current_time) / 60 * 100) / 100)
+
+        #Cleans up fulfillment text
+        
         is_stop = distance.find('stops')
         if stops <= 0:
             result = {'fulfillmentText': "The bus is " + str(distance)}
+            
         elif is_stop != -1:
-            result = {'fulfillmentText': "The bus is " + str(stops) + " stops away"}
+            result = {'fulfillmentText': "The bus is " + str(eta) + " minutes away or " + str(stops) + " stops away"}            
+
         else:
-            result = {'fulfillmentText': "The bus is " + str(distance) + " or " + str(stops) + " stops away"}
-        
+            result = {'fulfillmentText': "The bus is " + str(eta) + " minutes away or " + str(distance) + " and " + str(stops) + " stops away"}
+
         return jsonify(result)
 
 
@@ -53,21 +68,15 @@ def index():
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    payload = {'key':'3a03f90d-7ca3-4b74-b22f-8b6535e6d788','OperatorRef':'MTA','MonitoringRef':'306582','Li':'NYCT_B11','StopMonitoringDetailLevel':'basic','MaximumStopVisits':'1','MinimumStopVisitsPerLine':'1'}
+    obj = requests.get('http://bustime.mta.info/api/siri/stop-monitoring.json', params=payload)
+    res = obj.json()
+    busTime = res['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']["ExpectedArrivalTime"]
+    arrival_time = time.mktime(iso8601.parse_date(busTime).timetuple())
+    current_time = time.mktime(datetime.datetime.now().timetuple())
+    eta = math.floor(math.floor((arrival_time - current_time) / 60 * 100) / 100)
+    return render_template('index.html', eta = eta)
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", debug=True, port=8080)
-# [END python_app]
-
-"""    
-@app.route("/2")
-def get_request2():
-    payloadStops = {'key':'3a03f90d-7ca3-4b74-b22f-8b6535e6d788','OperatorRef':'MTA','MonitoringRef':'306582','Li':'NYCT_B11','StopMonitoringDetailLevel':'basic','MaximumStopVisits':'1','MinimumStopVisitsPerLine':'1'}
-    objStops = requests.get('http://bustime.mta.info/api/siri/stop-monitoring.json', params=payload)
-    resStops = obj.json()
-    stops = res['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['StopsFromCall']
-    return str(stops)
-    
-"""
-
+    app.run("0.0.0.0", debug=True, port=8080)
 
